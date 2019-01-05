1a6ec4270c5637177cc1ded4291e3185b74353b2
voidint <voidint@126.com>
2019-01-05 16:24:14 +0800

    Modify copyright information.

--- main.go
+++ main.go
@@ -71,7 +71,7 @@ func main() {
 	app.Name = "tsdump"
 	app.Usage = "Database table structure dump tool."
 	app.Version = build.Version(shortVersion)
-	app.Copyright = "Copyright (c) 2017, 2018, voidint. All rights reserved."
+	app.Copyright = "Copyright (c) 2017, 2019, voidint. All rights reserved."
 	app.Authors = []cli.Author{
 		cli.Author{
 			Name:  "voidnt",
